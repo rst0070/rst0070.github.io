9dfe18de63d92b18087ac7e04617039474d67667
Wonbin Kim <kwb0711@gmail.com>
2025-07-09 17:30:06 +0900

    Update about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
     <section className="max-w-lg mx-auto">
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">rst0070</h1>
       <p className="mb-4">
-      I'm a Software Engineer specializing in Data Engineering and Artificial Intelligence. I enjoy tackling complex problems and continuously learning new technologies. 
-      </p>
-      <p>
-      I'm trying to share my learnings and thoughts here. 
+      Software Engineer, trying to share my learnings and thoughts here. 
       </p>
     </section>
   );
